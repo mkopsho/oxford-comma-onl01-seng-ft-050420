e1d591b70b6f94e5e67b99fe95faf6edf687af5c
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-06 17:19:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
-  array.join(", ")
-  last_element = array.pop
-  last_element = ", and " + last_element
-  array << last_element
+  if array.length == 1
+    array.join(", ")
+  else
+    array.join(", ")
+    last_element = array.pop
+    last_element = ", and " + last_element
+    array << last_element
 end
